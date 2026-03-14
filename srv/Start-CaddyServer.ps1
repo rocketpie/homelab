f342048b961d3efc9cdc8d250916be5070029691
rocketpie <mail.l.conrad@gmail.com>
2026-03-14 15:05:16 +0100

    make caddy log to stdout

--- srv/Start-CaddyServer.ps1
+++ srv/Start-CaddyServer.ps1
@@ -1,3 +1,5 @@
+#Requires -Version 7
 Push-Location $PSScriptRoot
-.\caddy_windows_amd64.exe start
+#.\caddy_windows_amd64.exe start   # run in background
+.\caddy_windows_amd64.exe run     # run foreground/blocking
 Pop-Location
