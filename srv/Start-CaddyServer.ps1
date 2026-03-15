4d33e4687d880ff846a42900ee1ebb9bc9c730c1
rocketpie <mail.l.conrad@gmail.com>
2026-03-15 12:17:03 +0100

    debug provision.yml

--- srv/Start-CaddyServer.ps1
+++ srv/Start-CaddyServer.ps1
@@ -1,5 +1,5 @@
 #Requires -Version 7
 Push-Location $PSScriptRoot
-#.\caddy_windows_amd64.exe start   # run in background
-.\caddy_windows_amd64.exe run     # run foreground/blocking
+.\caddy_windows_amd64.exe start   # run in background
+#.\caddy_windows_amd64.exe run     # run foreground/blocking
 Pop-Location
